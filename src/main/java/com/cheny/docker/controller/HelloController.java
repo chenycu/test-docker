e1e37d7e12db9069f822c4692ef5fc21920f0e5e
cheny <you92@vip.qq.com>
2020-09-06 01:00:08 +0800

    测试负载均衡

--- src/main/java/com/cheny/docker/controller/HelloController.java
+++ src/main/java/com/cheny/docker/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.cheny.docker.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "hello")
 public class HelloController {
 
+    @Value("${server.port}")
+    private String port;
+
     @GetMapping("/desc")
     String hello() {
-        return "hello docker I am cheny bbb";
+        return "hello docker I am cheny port = " + port;
     }
 }
